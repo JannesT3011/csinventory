017520438d1bf36db0e50736dac24772d97be1bd
Bmbus <jannes.t13@gmail.com>
2020-06-13 13:20:46 +0200

    Finish item dict and return value in api

--- src/blueprints/api.py
+++ src/blueprints/api.py
@@ -2,11 +2,13 @@ from flask import Blueprint, jsonify, request
 import json
 from urllib.request import urlopen
 from steampy.client import SteamClient
-from steampy.utils import GameOptions
+#from steampy.utils import GameOptions
+from steampy.models import Currency, GameOptions
 import time
+import urllib
 
 api = Blueprint("api", __name__, url_prefix="/api")
-steam_client = SteamClient("")
+steam_client = SteamClient("D13799E79A69DE038BB9A50AD1703129")
 
 @api.route("/inventory/<steamid>")
 def get_inventory(steamid):
@@ -15,18 +17,33 @@ def get_inventory(steamid):
     data = urlopen('http://steamcommunity.com/profiles/'+steamid+'/inventory/json/730/2')
     json_data = json.loads(data.read())
     descriptions = json_data['rgDescriptions']
-    inv =  [descriptions[v]['market_hash_name'] for v in descriptions]
-    x = {}
+    inv =  [descriptions[v] for v in descriptions]
+    items = {}
+    number_inv = []
+    amount = {}
+    for value in json_data["rgInventory"]:
+        number_inv.append(json_data["rgInventory"][value]["classid"]) # TODO key class id benutzen! nochmal die row json angucken
+    for iid in number_inv:
+        amount[iid] = number_inv.count(iid)
+    #print(amount)
     for item in inv:
+        item_ = item["market_hash_name"]
         try:
-            if item.startswith("Sealed") or item.startswith("Graffiti") or item.endswith("Medal"):
+            if item_.startswith("Sealed") or item_.startswith("Graffiti") or item_.endswith("Medal") or item_.startswith("Storage") or item_.endswith("Badge"):
                 pass
             else:
-                x[item] = steam_client.market.fetch_price(item, GameOptions.CS)
+                #items[item_]["amount"] = amount[item["classid"]]
+                #print(item["classid"])
+                items[item_] = steam_client.market.fetch_price(item_, GameOptions.CS, currency=Currency.EURO) # die möglichkeit in euro umzurechnen
+                items[item_]["amount"] = amount[item["classid"]]
+                # TODO items dict in db speichern, dieser wird nur einmal am Tag gerefresht
         except:
+            print("sleep...")
             time.sleep(60)
-    print(x)
+            #print(e)
+            
+    #print(x)
 
-    return jsonify(x), 200 # TODO geht!, aber nur x returnen
+    return jsonify(items), 200 # TODO geht!, aber nur x returnen
 
     # TODO anuahl bekommt man mit der id eines items (sw-case: 519977179) und dann durch einen loop in rgInventory -> also einmal durch rgInventory loope, werte in einer liste speicher, diese werte zählen und dann den namen rausfinden
