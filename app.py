2594ac9ac4e076ed4d55b1bc15167297758ed5f0
Bmbus <jannes.t13@gmail.com>
2020-06-15 20:06:49 +0200

    Add errorhandler, inventory route+handler

--- app.py
+++ app.py
@@ -1,21 +1,23 @@
 from flask import Flask, request, render_template
 from src.blueprints.api import api
+from src.blueprints.error import error
 
 app = Flask(__name__)
 app.register_blueprint(api)
+app.register_blueprint(error)
 
 @app.route("/")
 def index():
-	return "hello world"
+	return render_template("index.html")
 
-@app.route("/inventory", methods=["GET", "POST"])
+@app.route("/inventory", methods=["POST"])
 def inventory():
-	steamid = request.form.get("search-id")
+	steamid = request.form["search-id"]
 	body = {
 		"steamid": steamid,
 		"api_key": ""
 	}
-	return "Handle search id form and return inv from db"
+	return body
 
 if __name__ == "__main__":
 	app.run(port=8080, debug=True, threaded=True)
---
+++ src/blueprints/error.py
@@ -0,0 +1,11 @@
+from flask import Blueprint, redirect, url_for
+
+error = Blueprint("error", __name__, url_prefix="/error")
+
+@error.app_errorhandler(404)
+def hanlde_404_error(e):
+    return "Not found!", 404
+
+@error.app_errorhandler(405)
+def hanlde_405_error(e):
+    return redirect("/"), 200
