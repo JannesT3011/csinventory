671ad07c89d79c0514f74078cbcc4704317db537
Bmbus <jannes.t13@gmail.com>
2020-06-22 15:41:52 +0200

    Add/Init react frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom"
+
+function App() {
+  return (
+    <div className="App">
+    
+    </div>
+  );
+}
+
+export default App;
