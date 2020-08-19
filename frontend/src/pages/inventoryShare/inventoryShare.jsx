2b3d1eebf33ac27a69e49025c2e6426d44c42d1b
Jannes <36892200+Bmbus@users.noreply.github.com>
2020-08-19 12:09:10 +0200

    Add copy button and link
    TODO: Copy function

--- frontend/src/pages/inventoryShare/inventoryShare.jsx
+++ frontend/src/pages/inventoryShare/inventoryShare.jsx
@@ -5,16 +5,23 @@ import Navbar from "../../components/navbar/navbar"
 import InvBar from "../../components/invNavBar/invNavbar"
 
 class InventoryShare extends React.Component {
+    copyLink = () => {
+
+    }
+
     render() {
         return(
             <div className="Share" align="center">
                 <Navbar title="History" steamid={this.props.match.params.steamid}/>
+                <br/>
                 <InvBar steamid={this.props.match.params.steamid}/>
                 <br/>
                 <br/>
                 <br/>
-                <br/>
-                <QRCode value={window.location.href} className="Qr-code" size="256"/>
+                <h4>Share your Inventory with your friends</h4>
+                <QRCode value={window.location.href.replace("share/", "")} className="Qr-code" size="256"/>
+                <h4 id="share-link">{window.location.href.replace("share/", "")}</h4>
+                <button onClick={this.copyLink} id="copy-button">Copy</button>
             </div>
         )
     }
