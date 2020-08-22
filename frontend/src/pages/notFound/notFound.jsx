173b6f352087c0d450fbe01440866589826905f1
Jannes <36892200+Bmbus@users.noreply.github.com>
2020-08-22 20:54:03 +0200

    Style notFound page

--- frontend/src/pages/notFound/notFound.jsx
+++ frontend/src/pages/notFound/notFound.jsx
@@ -1,14 +1,20 @@
 import React from "react"
 import {Link} from "react-router-dom"
+import "./notFound.scss"
 
 class NotFound extends React.Component {
     render() {
         return(
             <div className="NotFound">
-                <h1>404</h1>
-                <Link to="/">
-                    Back home
-                </Link>
+                <div class="NotFound-middle">
+                  <div class="NotFount-inner">
+                    <h4>404</h4>
+                    <h1>Page not found</h1>
+                    <hr/>
+                    <br/>
+                    <a href="/">Go back home!</a>
+                  </div>
+                </div>
             </div>
         )
     }
