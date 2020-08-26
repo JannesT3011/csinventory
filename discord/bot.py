13b22247ff44ac33bc132ff1419c3f64d08ab381
Bmbus <jannes.t13@gmail.com>
2020-08-26 10:09:43 +0200

    Init discord bot

---
+++ discord/bot.py
@@ -0,0 +1,33 @@
+from discord.ext import commands
+import discord
+
+#async def run():
+#    bot = CSInventoryDiscord()
+#    try:
+#        await bot.start("")
+#    except Exception as err:
+#        print(err)
+
+class CSInventoryDiscord(commands.AutoShardedBot):
+    def __init__(self, **kwargs):
+        super().__init__(
+            command_prefix=commands.when_mentioned_or("."),
+            description="discord bot",
+        )
+        self.creator = "Bmbus#8446"
+        self.github_url = "https://github.com/Bmbus/csinventory"
+        self.db = ""
+
+        self.load_extension("cogs.dcinventory")
+    
+    async def on_ready(self):
+        print("##########\n"f"{self.user.name}\n"f"{self.user.id}\n""##########")
+        print(discord.utils.oauth_url(self.user.id))
+    
+    async def on_message(self, message):
+        if message.guild:
+            return
+        await self.process_commands(message)
+
+if __name__ == "__main__":
+    CSInventoryDiscord().run("")
